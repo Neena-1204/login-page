fbe029174a351dd2f09c369b4179bd8f0f97ddc7
Neena <95159567+Neena-1204@users.noreply.github.com>
2023-09-14 08:47:23 +0530

    Add files via upload

---
+++ passport-config.js
@@ -0,0 +1,35 @@
+const LocalStrategy = require("passport-local").Strategy
+const bcrypt = require("bcrypt")
+
+
+function initialize(passport, getUserByEmail, getUserById){
+   // func fro auth
+   const authenticateUsers = async (email, password, done) => {
+    // get users by email
+    const user = getUserByEmail(email)
+    if (user == null){
+        return done(null, false, {message: "no user found in the email"})
+    }
+    try{
+        // compare passwords using bcypt
+        if (await bcrypt.compare(password, user.password)){
+            return done(null,user)
+        }
+         else{
+            return done (null, false, {message: "password incorrect"})
+        } 
+    }catch(e){ 
+        console.log(e)
+        return done(e)
+    }
+   }
+
+   passport.use(new LocalStrategy({usernameField: 'email'}, authenticateUsers))
+   passport.serializeUser((user, done) => done(null, user.id)) 
+   passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id))
+   }) 
+
+}
+
+module.exports = initialize
